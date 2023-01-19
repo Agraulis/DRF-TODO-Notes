36c86dcc0dd0dde26cf597fbaa592da856b757a3
Denis Roshchupkin <denis_roshypkin@mail.ru>
2023-01-19 21:27:02 +0500

    viewsets modified, paginaion added, filters added

--- ToDoNotes/ToDoApp/views.py
+++ ToDoNotes/ToDoApp/views.py
@@ -1,13 +1,42 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.pagination import LimitOffsetPagination
+
 from .models import Project, ToDoNote
 from .serializers import ProjectSerializer, ToDoNoteSerializer
+from .filters import ProjectFilter, ToDoNoteFilter
+
+import datetime
+
+
+class ProjectLimitOffsetPaginationViewSet(LimitOffsetPagination):
+    default_limit = 10
+
+
+class ToDoNoteLimitOffsetPaginationViewSet(LimitOffsetPagination):
+    default_limit = 20
 
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    pagination_class = ProjectLimitOffsetPaginationViewSet
+    filterset_class = ProjectFilter
 
 
 class ToDoNoteModelViewSet(ModelViewSet):
     queryset = ToDoNote.objects.all()
     serializer_class = ToDoNoteSerializer
+    pagination_class = ToDoNoteLimitOffsetPaginationViewSet
+    filterset_class = ToDoNoteFilter
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance,
+                                         data={'is_active': False,
+                                               'created_on': datetime.datetime.today(),
+                                               'updated_on': datetime.datetime.today()},
+                                         partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
