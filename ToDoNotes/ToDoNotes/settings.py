e110866be3623760221d9718af2417f2cab9d66f
Denis Roshchupkin <denis_roshypkin@mail.ru>
2023-01-26 20:44:52 +0500

    jwt token authentication added

--- ToDoNotes/ToDoNotes/settings.py
+++ ToDoNotes/ToDoNotes/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'django_filters',
     'corsheaders',
+    'rest_framework_jwt',
+    'rest_framework_jwt.blacklist',
 
     'users',
     'ToDoApp',
@@ -139,6 +141,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
--- ToDoNotes/ToDoNotes/urls.py
+++ ToDoNotes/ToDoNotes/urls.py
@@ -19,6 +19,7 @@ from rest_framework.routers import DefaultRouter
 from users.views import UserModelViewSet
 from ToDoApp.views import ProjectModelViewSet, ToDoNoteModelViewSet
 from rest_framework.authtoken import views
+from rest_framework_jwt.views import obtain_jwt_token
 
 router = DefaultRouter()
 router.register('users', UserModelViewSet)
@@ -29,5 +30,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+    path(r'^api-token-auth/', obtain_jwt_token),
     path('api-token-auth/', views.obtain_auth_token),
 ]
