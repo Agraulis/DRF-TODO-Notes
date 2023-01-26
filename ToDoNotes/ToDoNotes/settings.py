d51d524331de6963fc1f4d7e85f52b154639f7ce
Denis Roshchupkin <denis_roshypkin@mail.ru>
2023-01-26 20:11:37 +0500

    authentication and authorization added

--- ToDoNotes/ToDoNotes/settings.py
+++ ToDoNotes/ToDoNotes/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework.authtoken',
     'django_filters',
-    'users',
     'corsheaders',
+
+    'users',
     'ToDoApp',
 ]
 
@@ -133,6 +135,13 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
