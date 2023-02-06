b183eec612d9b8d1c48fe974c8ee066de0c42582
Denis Roshchupkin <denis_roshypkin@mail.ru>
2023-02-06 19:53:41 +0500

    GraphQL scheme added

--- ToDoNotes/ToDoNotes/settings.py
+++ ToDoNotes/ToDoNotes/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework_jwt',
     'rest_framework_jwt.blacklist',
     'drf_yasg',
+    'graphene_django',
 
     'users',
     'ToDoApp',
@@ -162,4 +163,8 @@ REST_FRAMEWORK = {
         'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
         'djangorestframework_camel_case.parser.CamelCaseJSONParser',
     ),
+}
+
+GRAPHENE = {
+    "SCHEMA": "ToDoNotes.schema.schema"
 }
--- ToDoNotes/ToDoNotes/urls.py
+++ ToDoNotes/ToDoNotes/urls.py
@@ -24,6 +24,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from graphene_django.views import GraphQLView
+
 from users.views import UserModelViewSet
 from ToDoApp.views import ProjectModelViewSet, ToDoNoteModelViewSet
 
@@ -57,4 +59,5 @@ urlpatterns = [
     name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
     name='schema-redoc'),
+    path("graphql/", GraphQLView.as_view(graphiql=True)),
 ]
